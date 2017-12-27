506cb463e1798d1b83e38ae7e0efbc29ca22b2e0
scroollocker <scroollocker@gmail.com>
2017-12-28 00:26:59 +0600

    fix datetimepicker missed module again

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,7 +7,7 @@
     <script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
     <script src="{{ asset('locales/bootstrap-datepicker.ru.min.js') }}"></script>
 
-    <script src="http://exchange.dev/js/angular-datetimepicker-directive.js"></script>
+    <script src="{{ asset('js/angular-datetimepicker-directive.js') }}"></script>
 
     <script src="{{ asset('js/app/admin.route.js') }}"></script>
     <script src="{{ asset('js/app/controllers/admin/user.js') }}"></script>
